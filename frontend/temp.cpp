e31d870c083a70ee2bcea08b08185a3deff1d06f
Anubhavpandey27 <anubhavpandey.bt22cseds@pec.edu.in>
2025-03-04 00:44:57 +0530

    update

---
+++ frontend/temp.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <math.h>
+#define ll long long
+//#include <ext/pb_ds/assoc_container.hpp> #include <ext/pb_ds/tree_policy.hpp> using namespace __gnu_pbds; #define ordered_set tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
+void helper(){
+}
+vector<ll>input(int n){
+  
+    vector<ll>vec;
+    for(int i=0;i<n;i++){
+      ll ele;
+      cin>>ele;
+      vec.emplace_back(ele);
+    }
+    return vec;
+}
+
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(NULL);
+int t;
+cin>>t;
+while(t){
+
+ t--;}
+
+}
